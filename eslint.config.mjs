6c50c35d341f0daaf1a11a5bd3658c686359aa8b
stsTom <tamilaustsiapan@gmail.com>
2026-09-15 18:58:42 +0200

    init: install and set up basic dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+// @ts-check
+
+import js from '@eslint/js';
+import eslintPluginUnicorn from 'eslint-plugin-unicorn';
+import tseslint from 'typescript-eslint';
+import eslintConfigPrettier from 'eslint-config-prettier/flat';
+
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintPluginUnicorn.configs.recommended,
+  eslintConfigPrettier,
+
+  {
+    linterOptions: {
+      noInlineConfig: true,
+    },
+  },
+
+  {
+    files: ['**/*.{js,ts}'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+    },
+  },
+];
