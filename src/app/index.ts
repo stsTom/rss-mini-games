5afe3fbf18b4089d10ca4f5ee16569764794fd20
stsTom <tamilaustsiapan@gmail.com>
2026-09-19 22:57:47 +0200

    fix: fix incorrect column spacing

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,10 +1,12 @@
 import { createHeader } from '../shared/features/header/header.js';
 import { createHero } from '../pages/home/features/hero/hero.js';
 import { createGamesCarousel } from '../pages/home/features/games-carousel/games-carousel.js';
+import { createLeaderboard } from '../pages/home/features/leaderboard/leaderboard.js';
 
 const main = document.querySelector('main');
 
 if (main) {
   main.append(createHeader(), createHero());
   main.append(await createGamesCarousel());
+  main.append(await createLeaderboard());
 }
