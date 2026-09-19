6852e9e8ad123b188b7095882a0474ec934c7782
stsTom <tamilaustsiapan@gmail.com>
2026-09-19 20:11:13 +0200

    refactor: add interfaces.ts

--- src/pages/home/games-carousel/games-data/games-data.ts
+++ src/pages/home/games-carousel/games-data/games-data.ts
@@ -1,16 +1,5 @@
 import type { SlotRole } from '../slot/slot.js';
-
-export interface Game {
-  slug: string;
-  name: string;
-  category: string;
-  price: string;
-  shortDescription: string;
-  rating: number;
-  likesCount: number;
-  cardImage: string;
-  featured: boolean;
-}
+import type { Game } from '../../../../shared/interfaces.js';
 
 interface GamesSeedResponse {
   data: Game[];
