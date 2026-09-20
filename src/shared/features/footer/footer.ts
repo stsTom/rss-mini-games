5b6b391cd3c29d609c399a5a7bf3593d61dcb384
stsTom <tamilaustsiapan@gmail.com>
2026-09-20 11:57:21 +0200

    feat: add footer tagline

--- src/shared/features/footer/footer.ts
+++ src/shared/features/footer/footer.ts
@@ -1,10 +1,21 @@
 import './footer.scss';
+import { createFooterHeading } from './components/heading/heading.js';
+import { createFooterTagline } from './components/tagline/tagline.js';
+
+function createBrandBlock(): HTMLElement {
+  const brandBlock = document.createElement('div');
+  brandBlock.classList.add('brand-block');
+  brandBlock.append(createFooterHeading(), createFooterTagline());
+
+  return brandBlock;
+}
 
 export function createFooter(): HTMLElement {
   const footer = document.createElement('footer');
 
   const upperFooter = document.createElement('div');
   upperFooter.classList.add('upper-footer');
+  upperFooter.append(createBrandBlock());
 
   const lowerFooter = document.createElement('div');
   lowerFooter.classList.add('lower-footer');
