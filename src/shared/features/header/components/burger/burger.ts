c512c2831367bb60a3642aea73b9cd105e2051fa
stsTom <tamilaustsiapan@gmail.com>
2026-09-21 15:04:45 +0200

    feat: style burger button

--- src/shared/features/header/components/burger/burger.ts
+++ src/shared/features/header/components/burger/burger.ts
@@ -5,5 +5,11 @@ export function createBurger(): HTMLElement {
   burger.type = 'button';
   burger.dataset.headerVisibility = 'mobile';
 
+  for (let index = 0; index < 3; index += 1) {
+    const line = document.createElement('span');
+    line.classList.add('burger-line');
+    burger.append(line);
+  }
+
   return burger;
 }
